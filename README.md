724a67a0c99a93cb626928bd697d81a6cb4312cc
William Yoon <wyoon@trentonsystems.com>
2022-07-14 17:02:04 -0400

    added some more metrics

--- README.md
+++ README.md
@@ -3,3 +3,5 @@ https://analyzingalpha.com/yfinance-python
 https://pypi.org/project/yfinance/
 https://www.learndatasci.com/tutorials/python-finance-part-3-moving-average-trading-strategy/ 
 https://www.analyticsvidhya.com/blog/2021/07/stock-prices-analysis-with-python/
+https://algotrading101.com/learn/yfinance-guide/
+https://www.fool.com/investing/2018/03/21/9-essential-metrics-all-smart-investors-should-kno.aspx
--- StockView-main/metrics.py
+++ StockView-main/metrics.py
@@ -7,9 +7,28 @@ import yfinance as yf # Yahoo Finance stock data
 import tkinter
 
 %matplotlib inline
+# dict_keys(['zip', 'sector', 'fullTimeEmployees', 'longBusinessSummary', 'city', 'phone', 'state', 'country', 'companyOfficers', 'website', 
+# 'maxAge', 'address1', 'fax', 'industry', 'address2', 'ebitdaMargins', 'profitMargins', 'grossMargins', 'operatingCashflow', 'revenueGrowth', 
+# 'operatingMargins', 'ebitda', 'targetLowPrice', 'recommendationKey', 'grossProfits', 'freeCashflow', 'targetMedianPrice', 'currentPrice', 
+# 'earningsGrowth', 'currentRatio', 'returnOnAssets', 'numberOfAnalystOpinions', 'targetMeanPrice', 'debtToEquity', 'returnOnEquity', 'targetHighPrice', 
+# 'totalCash', 'totalDebt', 'totalRevenue', 'totalCashPerShare', 'financialCurrency', 'revenuePerShare', 'quickRatio', 'recommendationMean', 'exchange', 
+# 'shortName', 'longName', 'exchangeTimezoneName', 'exchangeTimezoneShortName', 'isEsgPopulated', 'gmtOffSetMilliseconds', 'quoteType', 'symbol', 'messageBoardId', 
+# 'market', 'annualHoldingsTurnover', 'enterpriseToRevenue', 'beta3Year', 'enterpriseToEbitda', '52WeekChange', 'morningStarRiskRating', 'forwardEps', 
+# 'revenueQuarterlyGrowth', 'sharesOutstanding', 'fundInceptionDate', 'annualReportExpenseRatio', 'totalAssets', 'bookValue', 'sharesShort', 'sharesPercentSharesOut', 
+# 'fundFamily', 'lastFiscalYearEnd', 'heldPercentInstitutions', 'netIncomeToCommon', 'trailingEps', 'lastDividendValue', 'SandP52WeekChange', 'priceToBook', 
+# 'heldPercentInsiders', 'nextFiscalYearEnd', 'yield', 'mostRecentQuarter', 'shortRatio', 'sharesShortPreviousMonthDate', 'floatShares', 'beta', 'enterpriseValue', 
+# 'priceHint', 'threeYearAverageReturn', 'lastSplitDate', 'lastSplitFactor', 'legalType', 'lastDividendDate', 'morningStarOverallRating', 'earningsQuarterlyGrowth', 
+# 'priceToSalesTrailing12Months', 'dateShortInterest', 'pegRatio', 'ytdReturn', 'forwardPE', 'lastCapGain', 'shortPercentOfFloat', 'sharesShortPriorMonth', 
+# 'impliedSharesOutstanding', 'category', 'fiveYearAverageReturn', 'previousClose', 'regularMarketOpen', 'twoHundredDayAverage', 'trailingAnnualDividendYield', 
+# 'payoutRatio', 'volume24Hr', 'regularMarketDayHigh', 'navPrice', 'averageDailyVolume10Day', 'regularMarketPreviousClose', 'fiftyDayAverage', 'trailingAnnualDividendRate', 
+# 'open', 'toCurrency', 'averageVolume10days', 'expireDate', 'algorithm', 'dividendRate', 'exDividendDate', 'circulatingSupply', 'startDate', 'regularMarketDayLow', 
+# 'currency', 'trailingPE', 'regularMarketVolume', 'lastMarket', 'maxSupply', 'openInterest', 'marketCap', 'volumeAllCurrencies', 'strikePrice', 'averageVolume', 'dayLow', 
+# 'ask', 'askSize', 'volume', 'fiftyTwoWeekHigh', 'fromCurrency', 'fiveYearAvgDividendYield', 'fiftyTwoWeekLow', 'bid', 'tradeable', 'dividendYield', 'bidSize', 'dayHigh', 
+# 'regularMarketPrice', 'preMarketPrice', 'logo_url', 'trailingPegRatio'])
 
 class metrics:
     stock = None
+    stock_tick = None
 
     # default download
     def __init__(self, name):
@@ -17,10 +36,12 @@ class metrics:
         end = '2019-01-01'
 
         global stock # can now edit global stock
+        global stock_tick # can now edit global stock_tick
         
         # downloads 5 year data
         # open, close, high, low, volume
         stock = yf.download(name, start, end)
+        stock_tick = yf.Ticker(name)
 
     # stock fluctuation
     def fluctuation(self, name): 
@@ -34,12 +55,12 @@ class metrics:
         stock['MarketCap'].plot(label = name, figsize = (15,7))
 
     # 50-day moving average
-    def fifty_day_ma(self, name):
+    def fifty_day_ma(self):
         stock['MA50'] = stock['Open'].rolling(50).mean()
         stock['MA50'].plot()
 
     # 200-day moving average
-    def two_hundred_day_ma(self, name):
+    def two_hundred_day_ma(self):
         stock['MA200'] = stock['Open'].rolling(200).mean()
         stock['MA200'].plot()
 
@@ -56,4 +77,9 @@ class metrics:
         plt.legend
 
     # p/e ratio
-    def p_e_ratio(self, name):
+    def p_e_ratio(self):
+        stock_tick.info['forwardPE']
+    
+    def ped_ratiO(self):
+        stock_tick.inf0['forwardPE']
+    
